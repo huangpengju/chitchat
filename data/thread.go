c01f752461f9404233f659082c5424c9eb33880a
huangpengju <15713716933@163.com>
2023-08-01 22:33:53 +0800

    实现帖子作者、发布时间、评论数量等功能

--- data/thread.go
+++ data/thread.go
@@ -1,6 +1,9 @@
 // data 包的 thread.go 用于保存所有帖子相关代码
 // data 包除了包含与数据库交互的结构和代码，还包含了一些与数据处理密切相关的函数。
 // 包中 Threads 获取数据库中所有的帖子并返回
+// 包中 User 获取帖子的作者信息
+// 包中 CreatedAtDate 格式化帖子的CreatedAt日期，以便在屏幕上显示
+// 包中 NumReplies 获取一个帖子的评论数
 // 包中 CreateThread 创建一个新帖子
 // 包中 ThreadByUUID 通过UUID获取帖子
 // 包中 CreatePost 创建一个新的评论到一个帖子
@@ -43,6 +46,42 @@ func Threads() (threads []Thread, err error) {
 		threads = append(threads, conv)
 	}
 	// threads = []Thread{{Id: 1, Uuid: 1, Topic: "这是侯三", UserId: 1, CreatedAt: time.Now()}, {Id: 2, Uuid: 2, Topic: "李四李四", UserId: 2, CreatedAt: time.Now()}}
+	return
+}
+
+//  作者 {{ .User.Name }} - {{ .CreatedAtDate }} - {{ .NumReplies }} posts.
+
+// 获取一个帖子的作者
+// 方法的接收者：thread
+// 方法的返回值：user
+func (thread *Thread) User() (user User) {
+	user = User{}
+	Db.QueryRow("select id ,uuid,name,email,created_at from users where id = $1", thread.UserId).
+		Scan(&user.Id, &user.Uuid, &user.Name, &user.Email, &user.CreatedAt)
+	return
+}
+
+// CreatedAtDate 格式化CreatedAt日期，以便在屏幕上显示
+// 方法的接收者：thread
+// 返回值：字符串类型的格式化后的时间
+func (thread *Thread) CreatedAtDate() string {
+	return thread.CreatedAt.Format("2006/01/02 15:04")
+}
+
+// NumReplies 获取一个帖子的评论数
+// 方法的接收者：thread
+// 返回值：int类型的帖子的评论数量
+func (thread *Thread) NumReplies() (count int) {
+	rows, err := Db.Query("select count(*) from posts where thread_id = $1", thread.Id)
+	if err != nil {
+		return
+	}
+	for rows.Next() {
+		if err = rows.Scan(&count); err != nil {
+			return
+		}
+	}
+	rows.Close()
 	return
 }
 
